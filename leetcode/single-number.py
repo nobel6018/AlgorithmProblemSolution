e53c50fa61e1291800cc075d28df0477e4e4dcda
Lee, YoungHoon <nobel6018@gmail.com>
2022-04-20 13:56:28 +0900

    feat: single number

---
+++ leetcode/single-number.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/single-number/
+
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+
+        return result
