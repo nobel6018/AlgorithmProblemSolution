920a20947e48dd6f0e2388b8d13c4b82c7c905ce
Lee, YoungHoon <nobel6018@gmail.com>
2022-04-20 18:32:56 +0900

    feat: climbing staris #DynamicProgramming

---
+++ leetcode/climbing-stairs.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        steps = [0, 1, 2]
+        if n > 2:
+            for i in range(3, n + 1):
+                steps.append(steps[i - 1] + steps[i - 2])
+        return steps[n]
