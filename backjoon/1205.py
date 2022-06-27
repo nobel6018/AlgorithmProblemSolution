1a98e286fa6c484722148e2ea0f6335782969d4d
Lee, YoungHoon <nobel6018@gmail.com>
2022-06-27 23:45:07 +0900

    feat: baekjoon 1205 problem

---
+++ backjoon/1205.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/1205
+
+(n, score, p) = map(int, input().split(" "))
+
+# 예외 케이스 처리
+if n == 0:
+    print(1)
+    exit(0)
+
+scores = list(map(int, input().split(" ")))
+
+if n == p:
+    if scores[p - 1] >= score:
+        print(-1)
+        exit(0)
+
+grade = len(scores) + 1
+for i in range(len(scores)):
+    if scores[i] <= score:
+        grade = i + 1
+        break
+print(grade)
