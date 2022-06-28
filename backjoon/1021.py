dd0f165e4f63085544f4790bbe7c2ab98a342fd2
Lee, YoungHoon <nobel6018@gmail.com>
2022-06-28 20:32:28 +0900

    feat: baekjoon 1021 problem

---
+++ backjoon/1021.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/1021
+
+from collections import deque
+
+(n, m) = map(int, input().split(" "))
+removes = list(map(int, input().split(" ")))
+
+queue = deque()
+for i in range(n):
+    queue.append(i + 1)
+
+count = 0
+for remove in removes:
+    local_count = 0
+    while True:
+        item = queue.popleft()
+        if item == remove:
+            count += min(local_count, len(queue) + 1 - local_count)
+            break
+        else:
+            local_count += 1
+            queue.append(item)
+
+print(count)
