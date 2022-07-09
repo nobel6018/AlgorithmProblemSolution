f14d57ca706b8898db387a61e3d224f3248a9b5b
Lee, YoungHoon <nobel6018@gmail.com>
2022-07-09 14:57:27 +0900

    feat: baekjoon 1057 problem #greedy

---
+++ backjoon/1057.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1057
+
+(n, player1, player2) = map(int, input().split(" "))
+
+answer = 0
+while True:
+    answer += 1
+
+    player1 = (player1 // 2) + (player1 % 2)
+    player2 = (player2 // 2) + (player2 % 2)
+    if player1 == player2:
+        break
+
+    n = (n // 2) + (n % 2)
+
+print(answer)
