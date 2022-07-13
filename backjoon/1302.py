3fd2ae5ca680b20e5294a7711fcb60bd499a79b2
Lee, YoungHoon <nobel6018@gmail.com>
2022-07-13 12:01:03 +0900

    feat: baekjoon 1302 problem

---
+++ backjoon/1302.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/1302
+
+n = int(input())
+
+books = dict()
+for _ in range(n):
+    name = input()
+    if books.get(name, 0) == 0:
+        books[name] = 1
+    else:
+        books[name] += 1
+
+max_sold_count = 0
+max_sold_book_name = ""
+for book in books:
+    if max_sold_count < books[book]:
+        max_sold_count = max(max_sold_count, books[book])
+        max_sold_book_name = book
+    elif max_sold_count == books[book]:
+        max_sold_book_name = [max_sold_book_name, book]
+
+print(max_sold_book_name)
