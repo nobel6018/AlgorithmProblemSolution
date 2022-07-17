a536d80586d751fc5c10c184c4b33bfd640a970b
Lee, YoungHoon <nobel6018@gmail.com>
2022-07-18 08:12:58 +0900

    feat: bfs problem

---
+++ dongbin/5/11.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+
+def bfs(graph):
+    # row, col, step
+    queue = deque([(0, 0)])
+
+    # 아래, 위, 오른쪽, 왼쪽
+    dy = [1, -1, 0, 0]
+    dx = [0, 0, 1, -1]
+
+    while queue:
+        row, col = queue.popleft()
+        for i in range(4):
+            if row + dy[i] < 0 or row + dy[i] >= n or col + dx[i] < 0 or col + dx[i] >= m:
+                continue
+
+            if graph[row + dy[i]][col + dx[i]] == 1:
+                graph[row + dy[i]][col + dx[i]] = graph[row][col] + 1
+                queue.append((row + dy[i], col + dx[i]))
+
+    return graph[n - 1][m - 1]
+
+
+print(bfs(graph))
+
+# 5 6
+# 101010
+# 111111
+# 000001
+# 111111
+# 111111
