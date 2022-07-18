331151918fd1f4371ab60a1e237e9c594dbeb7a6
Lee, YoungHoon <nobel6018@gmail.com>
2022-07-18 21:42:39 +0900

    feat: update baekjoon 1057 problem

--- backjoon/1057.py
+++ backjoon/1057.py
@@ -1,16 +1,18 @@
 # https://www.acmicpc.net/problem/1057
 
-n, player1, player2 = map(int, input().split(" "))
+def next_number(num):
+    return (num // 2) + (num % 2)
+
+
+n, player1, player2 = map(int, input().split())
 
 answer = 0
 while True:
     answer += 1
 
-    player1 = (player1 // 2) + (player1 % 2)
-    player2 = (player2 // 2) + (player2 % 2)
+    player1 = next_number(player1)
+    player2 = next_number(player2)
     if player1 == player2:
         break
-
-    n = (n // 2) + (n % 2)
 
 print(answer)
