b327f8df0d74861d6afda230d70c1ed49a6f9afd
Lee, YoungHoon <nobel6018@gmail.com>
2022-07-26 17:20:50 +0900

    feat: update dijkstra algorithm - priority queue

--- study/dijkstra/dijkstra-priority-queue.py
+++ study/dijkstra/dijkstra-priority-queue.py
@@ -27,13 +27,14 @@ def dijkstra(start):
     distances[start] = 0
     while q:
         # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        distance, now = heapq.heappop(q)
+        current_distance, now = heapq.heappop(q)
         # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
-        if distances[now] < distance:
+        # visited 리스트를 따로 만들지 않고 이미 처리되었는 지 체크
+        if distances[now] < current_distance:
             continue
         # 현재 노드와 연결된 다른 인접한 노드들을 확인
         for i in graph[now]:
-            cost = distance + i[1]
+            cost = current_distance + i[1]
             # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
             if cost < distances[i[0]]:
                 distances[i[0]] = cost
