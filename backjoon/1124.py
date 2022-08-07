544e486e79f6acd14120ce4a559746c25b1433fa
Lee, YoungHoon <nobel6018@gmail.com>
2022-08-07 16:27:01 +0900

    feat: baekjoon 1124 problem - prime number, eratosthenes sieve

---
+++ backjoon/1124.py
@@ -0,0 +1,52 @@
+# https://www.acmicpc.net/problem/1124
+
+a, b = map(int, input().split())
+
+
+def eratosthenes_sieve(n):
+    sieve = [True] * (n + 1)
+    sieve[0] = False
+    sieve[1] = False
+
+    until = int(n ** 0.5)
+    for i in range(until + 1):
+        if sieve[i] is True:
+            for j in range(i + i, n + 1, i):
+                sieve[j] = False
+
+    return [i for i in range(n + 1) if sieve[i] is True]
+
+
+prime_numbers = eratosthenes_sieve(b)
+
+
+def is_prime(number: int) -> bool:
+    if number == 1:
+        return False
+
+    until = int(number ** 0.5)
+    for i in range(2, until + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def is_under_prime(prime_numbers, number):
+    count = 0
+    for prime in prime_numbers:
+        if number == 1:
+            break
+
+        while number % prime == 0:
+            count += 1
+            number //= prime
+
+    return is_prime(count)
+
+
+answer = 0
+for i in range(a, b + 1):
+    if is_under_prime(prime_numbers, i):
+        answer += 1
+
+print(answer)
