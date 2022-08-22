4302183f9a28010a27f9e311813e2e35ee090422
Lee, YoungHoon <nobel6018@gmail.com>
2022-08-23 08:04:02 +0900

    feat: solve forward direction loop

--- backjoon/1912.py
+++ backjoon/1912.py
@@ -4,16 +4,39 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-numbers = list(map(int, input().split()))
 
-total = [0] * n
-total[-1] = numbers[-1]
+def solution1():
+    n = int(input())
+    numbers = list(map(int, input().split()))
 
-for i in range(n - 2, -1, -1):
-    if total[i + 1] > 0:
-        total[i] = numbers[i] + total[i + 1]
-    else:
-        total[i] = numbers[i]
+    total = [0] * n
+    total[-1] = numbers[-1]
 
-print(max(total))
+    for i in range(n - 2, -1, -1):
+        if total[i + 1] > 0:
+            total[i] = numbers[i] + total[i + 1]
+        else:
+            total[i] = numbers[i]
+
+    print(max(total))
+
+
+def solution2():
+    n = int(input())
+    numbers = list(map(int, input().split()))
+
+    total = [0] * n
+    total[0] = numbers[0]
+
+    for i in range(1, n):
+        if total[i - 1] > 0:
+            total[i] = numbers[i] + total[i - 1]
+        else:
+            total[i] = numbers[i]
+
+    print(max(total))
+
+
+if __name__ == '__main__':
+    solution1()
+    # solution2()
