fcb67e565ff0d86b3ce478a9f87ff22e27fca88f
Lee, YoungHoon <nobel6018@gmail.com>
2023-02-03 08:20:17 +0900

    feat: baekjoon 1932 problem

--- backjoon/1932.py
+++ backjoon/1932.py
@@ -15,6 +15,6 @@ for i in range(1, n):
         elif j == i:
             nums[i][j] += nums[i - 1][j - 1]
         else:
-            nums[i][j] = max(nums[i][j] + nums[i - 1][j - 1], nums[i][j] + nums[i - 1][j])
+            nums[i][j] += max(nums[i - 1][j - 1], nums[i - 1][j])
 
 print(max(nums[n - 1]))
