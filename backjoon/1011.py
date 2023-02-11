e34e1bb8af2fa6eff0536fe5087a1be4547ac2d2
Lee, YoungHoon <nobel6018@gmail.com>
2023-02-12 00:15:15 +0900

    feat: baekjoon 1011 problem

--- backjoon/1011.py
+++ backjoon/1011.py
@@ -1,7 +1,4 @@
 import math
-import sys
-
-input = sys.stdin.readline
 
 n = int(input())
 
@@ -9,4 +6,15 @@ for _ in range(n):
     a, b = map(int, input().split())
     delta = b - a
 
-    int(math.sqrt(delta))
+    mod = int(math.sqrt(delta))
+    left = delta - mod ** 2
+    if left == 0:
+        print(2 * mod - 1)
+        continue
+    else:
+        half = ((mod + 1) ** 2 - mod ** 2) // 2
+        if half < left:
+            print(2 * mod - 1 + 2)
+            continue
+        else:
+            print(2 * mod - 1 + 1)
