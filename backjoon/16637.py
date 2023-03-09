0b27a8a934dddac7b8940b19f38aa1fdc6f207d4
Lee, YoungHoon <nobel6018@gmail.com>
2023-03-10 04:40:26 +0900

    feat: baekjoon 16637 problem - 괄호 안에 하나의 연산자 처리

--- backjoon/16637.py
+++ backjoon/16637.py
@@ -1,6 +1,10 @@
 n = int(input())
 m = input()
 
+if n == 1:
+    print(m)
+    exit(0)
+
 numbers = []
 operators = []
 for k, v in enumerate(m):
@@ -31,7 +35,7 @@ answer = -int(1e10)
 
 
 # 백트레이싱. 뒤에서부터 연산함
-def dfs(idx, numbers, operators):
+def dfs(idx, numbers, operators, visited):
     # 가장 마지막까지 왔음. 전체 계산하면 됨
     if idx == 0:
         global answer
@@ -39,12 +43,16 @@ def dfs(idx, numbers, operators):
         return
 
     # 괄호 쳐서 계산하는 거
-    new_numbers = numbers[:idx] + [operate(numbers[idx], numbers[idx + 1], operators[idx])] + numbers[idx + 2:]
-    new_operators = operators[:idx] + operators[idx + 1:]
-    dfs(idx - 1, new_numbers, new_operators)
+    if visited[idx + 1] is False:
+        new_numbers = numbers[:idx] + [operate(numbers[idx], numbers[idx + 1], operators[idx])] + numbers[idx + 2:]
+        new_operators = operators[:idx] + operators[idx + 1:]
+        new_visited = visited[:]
+        new_visited[idx] = True
+        dfs(idx - 1, new_numbers, new_operators, new_visited)
     # 괄호 안쳐서 계산하는 거
-    dfs(idx - 1, numbers, operators)
+    dfs(idx - 1, numbers, operators, visited[:])
 
 
-dfs(len(operators) - 1, numbers, operators)
+initial_index = len(operators) - 1
+dfs(initial_index, numbers, operators, [False] * (len(operators) + 1))
 print(answer)
